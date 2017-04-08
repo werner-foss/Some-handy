a340a1ae338861039336e97a5e594bea8b10ae08
Jordan Hudgens <wernerfosspilot@gmail.com>
2017-04-08 18:06:58 +0000

    Implemented concern for managing data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,11 +1,12 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
   scope :angular_portfolio, -> { where(subtitle:'Angular') }
   scope :rails_portfolio, -> { where(subtitle:'Ruby on Rails') }
   
   after_initialize :set_defaults
   def set_defaults
-    self.main_image ||= "https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=350&h=150"
-    self.thumb_image ||= "https://placeholdit.imgix.net/~text?txtsize=6&txt=50%C3%9750&w=50&h=50"
+    self.main_image ||= Placeholder.image_generator(height:'350',width:'150')
+    self.thumb_image ||= Placeholder.image_generator(height:'50',width:'50')
   end
 end
