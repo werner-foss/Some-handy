3d8568bab5c1017dca4b99a5b8030704acf13344
Jordan Hudgens <wernerfosspilot@gmail.com>
2017-04-15 15:57:44 +0000

    Refactored strong params for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,5 @@
 class Portfolio < ApplicationRecord
-  has_many :technologies 
+  has_many :technologies, dependent: :destroy 
   accepts_nested_attributes_for :technologies, reject_if: lambda {|attrs| attrs['name'].blank?}
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
