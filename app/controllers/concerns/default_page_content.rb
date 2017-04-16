2b5bcba34b29b7298c5fc3000f5abe1813b72b1f
Jordan Hudgens <wernerfosspilot@gmail.com>
2017-04-16 09:29:41 +0000

    Added nav bar and added default header content

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,10 @@
+module DefaultPageContent
+  extend ActiveSupport::Concern
+  included do
+    before_filter :set_page_defaults
+  end
+  def set_page_defaults
+    @page_title = "Portfolio | Werner Foss"
+    @seo_keywords = "Werner Foss portfolio"
+  end
+end
