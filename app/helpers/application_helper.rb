b1e020c1cf0fb44636a1a7968c7c07651a71e1ef
Jordan Hudgens <wernerfosspilot@gmail.com>
2017-04-26 13:54:54 +0000

    Styling footer, login and button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,15 @@
 module ApplicationHelper
-  def login_helper
+  def login_helper(style)
    unless current_user.is_a?(GuestUser)
-    link_to "Logout", destroy_user_session_path, method: :delete
+    link_to "Logout", destroy_user_session_path, method: :delete, class: style
    else
-    (link_to "Register", new_user_registration_path) +
-    "<br>".html_safe +
-    (link_to "Login", new_user_session_path)
+    (link_to "Register", new_user_registration_path, class: style) +
+    " ".html_safe +
+    (link_to "Login", new_user_session_path, class: style)
    end
   end
   
-  def source_helper
+  def source_helper(layout_name)
    if session[:source]
   greeting = "Thanks for visiting me from #{session[:source]}"
   content_tag(:p, greeting, class: "source_greating")
