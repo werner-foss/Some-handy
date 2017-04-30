e96ded787c74f353da45a5aa822661083076b4d5
Jordan Hudgens <wernerfosspilot@gmail.com>
2017-04-30 15:32:41 +0000

    Integrated main views for portfolios next new, edit and so on

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,7 +5,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.page(params[:page]).per(3)
     @page_title = "My Portfolio Blogs"
   end
 
