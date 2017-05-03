305fded9ba5f2f231cbb26fe2ffa5aa876138578
Jordan Hudgens <wernerfosspilot@gmail.com>
2017-05-03 17:50:19 +0000

    Integrated upload buttons for portfolios and a local mechanism

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,6 +3,10 @@ class Portfolio < ApplicationRecord
   accepts_nested_attributes_for :technologies, reject_if: lambda {|attrs| attrs['name'].blank?}
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
+  
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
+  
   scope :angular_portfolio, -> { where(subtitle:'Angular') }
   scope :rails_portfolio, -> { where(subtitle:'Ruby on Rails') }
   
