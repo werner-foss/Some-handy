07ea2eb025188c37fe198044c8218f5c7076ac8d
Jordan Hudgens <wernerfosspilot@gmail.com>
2017-05-04 14:40:35 +0000

    Refactored upload buttons, started changing default image placeholder

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,8 +1,7 @@
 class Portfolio < ApplicationRecord
   has_many :technologies, dependent: :destroy 
   accepts_nested_attributes_for :technologies, reject_if: lambda {|attrs| attrs['name'].blank?}
-  include Placeholder
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  validates_presence_of :title, :body
   
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
@@ -13,10 +12,5 @@ class Portfolio < ApplicationRecord
   def self.by_position
     order("position ASC")
   end
- 
-  after_initialize :set_defaults
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height:'350',width:'150')
-    self.thumb_image ||= Placeholder.image_generator(height:'50',width:'50')
-  end
+
 end
