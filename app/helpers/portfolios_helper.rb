87a61456c686597fc3e5ca135153e6cf0cf158ac
Jordan Hudgens <wernerfosspilot@gmail.com>
2017-05-05 15:28:35 +0000

    Finished with placeholder and made it working

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,14 +1,14 @@
 module PortfoliosHelper
-  def self.image_generator(height:, width:)
+  def image_generator(height:, width:)
     "https://placehold.it/#{height}x#{width}"
   end
   def portfolio_img img, type
-    if img
+    if img.model.main_image? || img.model.thumb_image?
       img
     elsif type == 'thumb'
-      image_generator(height: '350', width: '200')
+      image_generator(height:'350', width:'200')
     else
-      image_generator(height: '600', width: '400')
+      image_generator(height:'600', width:'400')
     end
   end
 end
