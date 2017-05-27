e303194b3ff0454eed9942c2009ce50910e0eef1
Jordan Hudgens <wernerfosspilot@gmail.com>
2017-05-27 17:17:26 +0000

    Final change for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_cable.allowed_request_origins = ['https://wf-portfolio.herokuapp.com/', 'http://wf-portfolio.herokuapp.com/']
+  config.action_cable.url = "wss://wf-portfolio.herokuapp.com/cable"
 end
